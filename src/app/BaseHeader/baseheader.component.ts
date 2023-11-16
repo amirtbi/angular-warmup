328344d418986a728271a57732732ecebc788ff2
amir <torabi@gmail.com>
2023-11-16 12:10:42 +0330

    add folders

---
+++ src/app/BaseHeader/baseheader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './baseheader.component.html',
+  styleUrls: ['./baseheader.component.css'],
+})
+export class BaseHeaderComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { BookingComponent } from './Shopping/booking.component';
+import { RecipeComponent } from './Recipe/recipe.component';
+import { BaseHeaderComponent } from './BaseHeader/baseheader.component';
+import { RecipeListComponent } from './Recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './Recipe/recipe-detail/recipe-detail.component';
+import { ShoppingEditComponent } from './Shopping/shopping-edit/shopping-edit.component';
+import { RecipeItemComponent } from './Recipe/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeItemComponent,
+    RecipeComponent,
+    BaseHeaderComponent,
+    BookingComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ShoppingEditComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
