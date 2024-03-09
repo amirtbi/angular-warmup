5e0dcb6b08cf444efd1b50d1378bad6864b0745c
amir <torabi46@gmail.com>
2024-03-09 13:50:31 +0330

    add recipe model and define get and post from /to firebase

---
+++ src/app/cores/recipe.mode.ts
@@ -0,0 +1,6 @@
+export interface IRecipe{
+    title:string;
+    image:string;
+    imageType:string
+    id?: string
+}
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from '../shared/recipes.service';
 import { ApiService } from '../shared/api.shared.service';
+import { HttpClient } from '@angular/common/http';
+import {map} from "rxjs/operators"
+import { IRecipe } from '../cores/recipe.mode';
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
@@ -9,6 +12,7 @@ import { ApiService } from '../shared/api.shared.service';
 export class RecipesComponent implements OnInit {
   recipes: { title: string; image: string; id: number }[] = [];
   filterText ="";
+  loading=false;
   currentDate = new Promise(resolve=>{
     setTimeout(()=>{
       resolve(new Date())
@@ -16,13 +20,32 @@ export class RecipesComponent implements OnInit {
   })
   constructor(
     private recipeService: RecipeService,
-    private apiService: ApiService
+    private apiService: ApiService,
+    private http:HttpClient
   ) {}
 
   customColumn() {
     const recipesLength = this.recipes.length;
     const columns = Math.ceil(recipesLength / 12);
     return `col-md-${columns}`;
+  }
+
+  onCreatePostData(){
+    this.http.post("https://ng-angulare-default-rtdb.firebaseio.com/post.json", this.recipes).subscribe();
+  }
+   onFetchData(){
+     this.loading = true;
+     this.http.get<{[key:string]:IRecipe}>("https://ng-angulare-default-rtdb.firebaseio.com/post.json").pipe(
+       map(responseData=>{
+         const resultArray: IRecipe[] = [];
+         for(const key in responseData){
+           if(responseData.hasOwnProperty(key)){
+             resultArray.push({ ...responseData[key],id:key})
+           }
+         }
+         return resultArray;
+       }
+      )).subscribe(data=>this.loading = false)
   }
   ngOnInit(): void {
     this.recipeService.getRecipes().subscribe((data: any) => {
